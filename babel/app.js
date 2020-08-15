71347d65ffb4bdcf180e3382e8b31af16aaaefc4
zhuming <1109282075@qq.com>
2020-08-15 12:19:57 +0800

    babel以及webpack

---
+++ babel/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+let n = 2**2
+
+// function observer(){
+//     @observer
+// }
+
+class App extends Component{
+    render(){
+        return(
+            // <div>
+            //     <img src="baidu.com" alt="baidu" className="box"/>
+            // </div>
+            React.createElement('div')
+        )
+    }
+}
