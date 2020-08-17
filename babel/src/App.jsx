81322ead5a3b128ec2e0c31f6a4af78d6fee4e33
zhuming <1109282075@qq.com>
2020-08-17 14:58:42 +0800

    babel

---
+++ babel/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function App(){
+    return (
+        <div>
+           <img src="/ip6x2.png" alt="图片加载出错"/>
+           吴彦祖你好
+        </div>
+    )
+}
--- babel/webpack.config.js
+++ babel/webpack.config.js
@@ -1,10 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+
 const config = {
     entry: './src/index.js',
     resolve: {
       extensions: ['.js', '.jsx']
     },
     devServer:{
-        contentBase:'./dist',
+        contentBase:'./public',
         hot:true
     },
     module: {
@@ -15,7 +19,21 @@ const config = {
           exclude: /node_modules/
         }
       ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template:'./public/index.html',
+        filenamw:'index.html'
+      }),
+      new BundleAnalyzerPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: '../public/ip6x2.png', to: './' },
+        ],
+      }),
+  
+    ]
   }
   module.exports = config
   
+  //dist 存着的是最终打包的结果
